8c218dbe4d165a9bf963dc0661c4e67180b0f871
Aleksander Nyczyk <olek@imdripfeed.com>
2020-08-25 10:25:08 +0200

    fixed for ie

--- src/index.php
+++ src/index.php
@@ -17,8 +17,10 @@
             <div class="col-sm-6 d-flex flex-column module-1__col1">
                 <h2>Long headline to turn your visitors into users</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet dolor id leo hendrerit malesuada vitae nec quam. Fusce quis purus vitae ligula pretium sollicitudin ac vitae lorem. Fusce interdum eget nunc a pellentesque. </p>
-                <h3>Feature</h3>
-                <p>Gorgeous, high-quality video sharing on desktop, mobile, tablet</p>
+                <div class="module-1__bottom">
+                    <h3>Feature</h3>
+                    <p>Gorgeous, high-quality video sharing on desktop, mobile, tablet</p>
+                </div>
             </div>
             <div class="col-sm-6 module-1__col2">
                 <div class="module-1__slider mb-0">
