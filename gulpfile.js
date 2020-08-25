6359730d24ac4d5a7f598f57185a1e284d4930ce
Aleksander Nyczyk <olek@imdripfeed.com>
2020-08-25 13:45:58 +0200

    added media

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+//compile scss into css
+function style() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+}
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./src",
+            index: "index.php"
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style)
+    gulp.watch('./*.php').on('change',browserSync.reload);
+    gulp.watch('./*.html').on('change',browserSync.reload);
+    gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
